aa7d8c656a2e8834b345f8d54edb68f7108333b0
mickBoat00 <m.boateng0000@gmail.com>
2024-01-17 17:30:05 +0000

    setup chi router for http services

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/mickBoat00/TransactionAPI
 
 go 1.21.5
+
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/joho/godotenv v1.5.1
+)
--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/joho/godotenv"
+	"github.com/mickBoat00/TransactionAPI/handlers"
+)
 
 func main() {
-	fmt.Println("fmt")
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error: %s", err)
+	}
+
+	PORT := os.Getenv("PORT")
+
+	if PORT == "" {
+		log.Fatal("Environment variable PORT is missing.")
+	}
+
+	router.Get("/", handlers.Home)
+
+	log.Printf("Server is started on PORT:%v", PORT)
+	http.ListenAndServe(fmt.Sprintf(":%s", PORT), router)
 }
